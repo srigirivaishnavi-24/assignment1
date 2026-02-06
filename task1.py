abb75344be91a94e60451a9c1cc835e34031cd25
srigirivaishnavi-24 <srigirivaishnavi@gmail.com>
2026-02-06 19:53:25 +0530

    assignment1

---
+++ task1.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+
+print("Addition:", num1+num2)
+print("Subtraction:", num1-num2)
+print("Multiplication:", num1*num2)
+print("Division:", num1/num2)
---
+++ task2.py
@@ -0,0 +1,4 @@
+first_name=input("enter your first name:")
+last_name=input("enter your last name:")
+
+print(f"Hello, {first_name} {last_name}! Welcome to the python program.")
